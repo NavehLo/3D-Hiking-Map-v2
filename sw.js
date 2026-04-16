b93379ad92ee0bad07bc70f6bd40decb6dde22ca
NavehLo <noovlp@gmail.com>
2026-04-16 18:46:46 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'trail3d-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // Intentionally not caching mapbox gl JS dynamically here to avoid huge size, 
+      // but caching our basic app shell
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Try network first for flexibility, fallback to cache if offline
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
